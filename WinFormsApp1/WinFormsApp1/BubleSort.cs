9426d864cccdf89e1c8e15b6b5f8cee3b6e8c33b
arthurcezars <arthurcezars@gmail.com>
2022-11-28 21:56:18 -0300

    Projeto Editor de Texto - Adicionado algoritmo de ordenação BubleSort

---
+++ WinFormsApp1/WinFormsApp1/BubleSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    internal class BubleSort
+    {
+        public string[] Sortear(string[] vetor)
+        {
+            bool trocou;
+            do
+            {
+                trocou = false;
+                for (int i = 0; i < vetor.Length - 1; i++)
+                {
+                    string p1 = vetor[i].ToLower();
+                    string p2 = vetor[i + 1].ToLower();
+                    if (string.Compare(p1, p2) == 1)
+                    {
+                        vetor[i] = p2;
+                        vetor[i + 1] = p1;
+                        trocou = true;
+                    }
+                }
+            } while (trocou);
+            return vetor;
+        }
+    }
+}
